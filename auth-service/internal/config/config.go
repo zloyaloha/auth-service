61e8fe697495490e3188d5a05a3c55ef403ffbef
zloyaloha <cutevf17@gmail.com>
2025-06-17 23:20:19 +0300

    now it is mono repo
    Signed-off-by: zloyaloha <cutevf17@gmail.com>

--- auth-service/internal/config/config.go
+++ auth-service/internal/config/config.go
@@ -10,7 +10,6 @@ import (
 
 type Config struct {
 	Env 			string			`yaml:"env" env-default:"local"`
-	// StoragePath 	string			`yaml:"storage_path" env-required:"true"`
 	PGConnectionConfig PGConnectionConfig `yaml:"pgconnection"`
 	GRPC 			GRPCConfig		`yaml:"grpc"`
 	MigrationsPath 	string
